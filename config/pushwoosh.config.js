9cd516f372432d0c8fc4d7b3e96228d1e10b0845
pxman1012 <pxman18011997@gmail.com>
2024-04-03 11:30:14 +0700

    divide pushwoosh config

--- config/pushwoosh.config.js
+++ config/pushwoosh.config.js
@@ -2,7 +2,8 @@ export const PUSHWOOSH_SCRIPT = `
     var Pushwoosh = window.Pushwoosh || [];
     Pushwoosh.push(['init', {
         logLevel: 'info', // possible values: error, info, debug
-        applicationCode: 'F1AF6-EDA8D', // you application code from Pushwoosh Control Panel
+        applicationCode: '78A77-3DF89', // you application code from Pushwoosh Control Panel
+        // applicationCode: 'F1AF6-EDA8D', // you application code from Pushwoosh Control Panel
         safariWebsitePushID: 'web.com.example.domain', //  unique reverse-domain string, obtained in you Apple Developer Portal. Only needed if you send push notifications to Safari browser
         defaultNotificationTitle: 'Pushwoosh', // sets a default title for push notifications
         defaultNotificationImage: 'https://yoursite.com/img/logo-medium.png', // URL to custom custom notification image
@@ -12,7 +13,7 @@ export const PUSHWOOSH_SCRIPT = `
         },
         userId: 'user_id', // optional, set custom user ID
         tags: {
-            'Name': 'Polsat Box Go'   	// optional, set custom Tags
+            'Name': 'PXMAN'   	// optional, set custom Tags
         }										
     }]);
 `;
